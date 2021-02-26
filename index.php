9cf5617892f1961ee76b286da5a105c3c381700f
sezaite <martyna.sezaite@gmail.com>
2021-02-26 17:02:26 +0000

    neveikia css??

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
+session_start();
 
-if(!empty($_POST)){
-    for($i = 1; $i <= count($_POST); $i++){
-        _d($_POST);
-        _d($_POST[$i]);
-        _d($_POST["$i"]);
-    
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    _d($_POST);
+    if (isset($_POST['boxes'])){
+        $boxuKiekis = count($_POST['boxes']);
+        $sum = 0;
+        for($i = 0; $i < $boxuKiekis; $i++){
+            $sum += $_POST['boxes'][$i];  
+        }    
+    }  else {
+        echo 'nieko nepasirinkai, seneliumbai';
     }
-header('Location : http://localhost/bit/ar-tu-storas-robotas/index.php');
-die;
-    
+    $_SESSION['sum'] = $sum;
+    $_SESSION['boxuKiekis'] = $boxuKiekis;
+    header('Location: http://localhost/bit/ar-tu-storas-robotas/');
+    die;
 }
+if(isset($_SESSION['sum'])){
+    if($_SESSION['sum'] === 22 && $_SESSION['boxuKiekis'] === 5){
+        echo 'Labas, Martyna';
+    } else if ($_SESSION['sum'] === 45 && $_SESSION['boxuKiekis'] === 9){
+        echo 'Tam storam robotui viskas skanu';
+    } else {
+        echo 'nezinau, nezinau, nepazistu taves';
+    }
+}
+session_destroy();
 
+    
 ?>
 
 <!DOCTYPE html>
@@ -24,12 +41,15 @@ die;
     <link rel="stylesheet" href="style.css">    
 </head>
 <body>
-
-<form action="index.php" method = "POST">
+<div>
+<h1>Prašome pasirinkti tik skanius patiekalus:</h1> 
+<img src="./img/main.jpg" alt="riesutu sviestas">
+</div>
+<form action="" method = "post">
 <?php
 $HTML = "";
 for($i = 1; $i < 10; $i++){
-$HTML .= "<input type='checkbox' name='$i' value='$i' id='$i'/><label for='$i'><img src='./img/$i.jpg'/></label>";
+$HTML .= "<input type='checkbox' name='boxes[]' value='$i' id='$i'/><label for='$i'><img src='./img/$i.jpg'/></label>";
 }
 echo $HTML;
 ?>
